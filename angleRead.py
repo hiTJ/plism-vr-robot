bb971082086c8a6fa2c8866bfed042949f003aa9
hiTJ <tojohiroyuki0511@yahoo.co.jp>
2022-03-10 23:05:11 +0000

    fix bug

--- angleRead.py
+++ angleRead.py
@@ -3,15 +3,15 @@ import subprocess
 from subprocess import PIPE
 import copy
 
-pos = None
-posold = None
+pos = [0,0]
+posold = [0,0]
 while(1):
 	f = open('xwiimote/angle.txt', 'r', encoding='UTF-8')
 	txt = f.read()
 	f.close()
 	print(txt)
 	pos = txt.split(',')
-	if(len(pos) > 1 and pos != posold):
+	if(len(pos) > 1 and (pos[0] != posold[0] or pos[1] != posold[1])):
 		#shell = '/Servo/Servo ' + pos[0] + ' ' + pos[1]
 		shell = '/home/pi/plism-vr-robot/Servo/Servo ' + pos[0] + ' ' + pos[1]
 		#print('mp_x = ' + pos[0] + ', mp_y = ' + pos[1])
